823f741492437d3982c738e73de44dd81662496c
sultan93291 <abibdipto@gmail.com>
2025-10-07 11:26:02 +0600

    feat: working on changes

--- src/components/DestinationDetails/DestinationPlacesToVisit.jsx
+++ src/components/DestinationDetails/DestinationPlacesToVisit.jsx
@@ -6,41 +6,23 @@ const DestinationPlacesToVisit = ({ placesToVisitInfo }) => {
   const imgBaseurl = import.meta.env.VITE_SERVER_URL;
   const { t } = useTranslation();
 
+  console.log(placesToVisitInfo, "this is the places info");
+
   return (
     <div
       id="places-to-visit"
       className="flex flex-col lg:my-0 2xl:flex-row md:gap-12 gap-10"
     >
-      {/* left side */}
-      <div className="hidden 2xl:flex flex-col gap-y-[95px] items-center 2xl:w-1/2">
+      <div className="hidden w-full 2xl:flex flex-col gap-y-[95px] items-center">
         <h4 className="text-3xl font-editorsNoteNormal text-primary">
           {t("destination1.mapTitle", { name: placesToVisitInfo?.name })}
         </h4>
-        <div className="w-full">
+        <div className="w-[650px] relative">
           <img
-            src={`${imgBaseurl}/${placesToVisitInfo.destination_details?.map}`}
+            src={`${imgBaseurl}/${placesToVisitInfo?.destination_details?.map}`}
             alt="not found"
-            className="h-full max-h-[814px] w-full object-cover"
+            className="object-cover w-full h-[400px] sm:h-[500px] md:h-[600px] lg:h-[700px] xl:h-[814px] max-h-[814px] rounded-lg"
           />
-        </div>
-      </div>
-
-      {/* right side */}
-      <div className="w-full 2xl:w-1/2">
-        {/* title */}
-        <h4 className="text-3xl font-editorsNoteNormal text-primary">
-          <Trans
-            i18nKey="destination1.topPlacesTitle"
-            values={{ name: placesToVisitInfo?.name }}
-            components={{ italic: <span className="font-editorsNoteItalic" /> }}
-          />
-        </h4>
-
-        {/* top places cards */}
-        <div className="space-y-5 md:mt-8 mt-4">
-          {placesToVisitInfo?.trip_packages?.slice(0, 3).map(item => (
-            <PlacesToVisitCard key={item?.title} item={item} />
-          ))}
         </div>
       </div>
     </div>
